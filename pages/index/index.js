c23063a8df97dc60309bd8a71cf075955a83068b
王向前 <95150209@qq.com>
2020-05-13 17:36:48 +0800

    nav开发（未完成）

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,6 +4,8 @@ Page({
   data: {
     // 轮播图数组
     swiperList: [],
+    // 导航数组
+    navList: [],
   },
   // 查询轮播图
   querySwiperList: function () {
@@ -15,7 +17,19 @@ Page({
       });
     });
   },
+  // 查询导航
+  queryNavList: function () {
+    request({
+      url: "https://api-hmugo-web.itheima.net/api/public/v1/home/catitems",
+    }).then((result) => {
+      console.log('导航数据', result);
+      this.setData({
+        navList: result.data.message
+      })
+    })
+  },
   onLoad: function () {
     this.querySwiperList();
+    this.queryNavList();
   },
 });
